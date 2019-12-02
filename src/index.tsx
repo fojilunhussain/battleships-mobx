0269b8412053200631ed120d103c2aa9453f7fc1
Fojilun Hussain <fojilun.hussain@investec.co.uk>
2019-12-02 14:02:51 +0000

    added interface for types and rewrote store as class

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,34 @@
-import { observable, autorun } from 'mobx'
+import { observable } from "mobx";
 
-const battleshipsStore = observable({
+interface IBattleshipsInterface {
+    playersBoard: string[],
+    computersBoard: string[],
+    ships: object,
+    hits: number,
+    misses: number,
+    turnsRemaining: number,
+    gameStarted: boolean
+}
+
+class BattleshipsStore {
+  @observable
+  battleshipsStore = {
     playersBoard: [],
     computersBoard: [],
     ships: {
-        Carrier: 5,
-        Battleship: 4,
-        Cruiser: 3,
-        Submarine: 3,
-        Destroyer: 2
+      Carrier: 5,
+      Battleship: 4,
+      Cruiser: 3,
+      Submarine: 3,
+      Destroyer: 2
     },
     hits: 0,
     misses: 100,
     turnsRemaining: 100,
     gameStarted: false
-})
+  };
+}
+
+var newGame = new BattleshipsStore
+
+console.log(newGame.battleshipsStore.hits)
